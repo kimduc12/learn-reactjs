46d2803d5f401ceb376b1d3058430a391a6149bb
Kim Duc <tranphammaikimduc@gmail.com>
2021-08-31 09:02:57 +0700

    Implement product image and constants

--- src/features/Product/components/ProductItem.jsx
+++ src/features/Product/components/ProductItem.jsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
+import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from 'constants/index';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -13,9 +14,12 @@ ProductItem.defaultProps = {
 
 function ProductItem(props) {
     const { product } = props;
+    const thumbnail = product.thumbnail ? `${STATIC_HOST}${product.thumbnail?.url}` : `${THUMBNAIL_PLACEHOLDER}`;
     return (
         <Box padding={1}>
-            <Skeleton variant="rect" width="100%" height={118} />
+            <Box padding={1}>
+                <img src={thumbnail} alt={product.name} width="100%" />
+            </Box>
             <Typography variant="body2">{product.name}</Typography>
             <Typography variant="body2">
                 {product.salePrice} - {product.promotionPercent}
